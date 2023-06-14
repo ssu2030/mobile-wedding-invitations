146aa2332b56b83d9f0fd14a571c2fbb9087c66c
dongho_lee <dongho_lee@tmax.co.kr>
2023-06-14 14:51:50 +0900

    [feat] 최적화된 비디오 추가
    - video tag내 source의 경우 여러개를 적어놓더라도 가장위부터 랜더를하고
    - 첫번째가 지원이안되면 두번째로 render하게된다.
    - 기존 mp4파일을 webm 타입으로 동영상 압축을 통한 최적화 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import introVideo_mp4 from '@video/intro_page_video.mp4'
+import introVideo_webm from '@video/intro_page_video.webm';
+
+
 
 const App: React.FC = () => {
-  return <div>청첩장</div>;
+  return <div>
+    <video autoPlay loop muted>
+      <source src={introVideo_webm} type="video/webm"/>
+      <source src={introVideo_mp4} type="video/mp4"/>
+    </video>
+  </div>;
 };
 
 export default App;
