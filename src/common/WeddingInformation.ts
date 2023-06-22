4de0c22a911cf282892e584fe347892bbfe30934
dongho_lee <dongho_lee@tmax.co.kr>
2023-06-22 10:58:30 +0900

    지도영역 컴포넌트 고도화 진행

---
+++ src/common/WeddingInformation.ts
@@ -0,0 +1,8 @@
+
+
+export const NamsanPhoneNumber: string = "02-795-2244";
+
+export const NamsanLatitude = 37.54460830000184;
+export const NamsanLongitude = 126.9963157539338; 
+
+export const NamsanAddr = "서울 용산구 소월로 375 (한남동 726-157)";
--- src/page/invitationDetail/InvitationMapComponent.tsx
+++ src/page/invitationDetail/InvitationMapComponent.tsx
@@ -1,33 +1,91 @@
 import React from "react";
 
-import styles from "style/contents/InvitationMap.module.scss"
-import  ImageComponent  from 'component/ImageComponent';
-import { contentResource } from 'common/DataTypes';
-import mapImage from '@photo/maptogo.webp';
+import ImageComponent from "component/ImageComponent";
+import { contentResource } from "common/DataTypes";
+import mapImage from "@photo/maptogo.webp";
+import hanMapImage from "@photo/handMap.webp";
 
-const InvitationMapComponent:React.FC = () => {
-    const contentResource: contentResource[] = [{
-        resourcePath: mapImage,
-        type:"image/webp"
-    }]
-    const openKakaoMapApp = () => {
-        const latitude = 37.5535; // 서울역 위도
-        const longitude = 126.9690; // 서울역 경도
-      
-        const kakaoMapAppUrl = `kakaomap://look?p=${latitude},${longitude}`;
-      
-        window.location.href = kakaoMapAppUrl;
-      };
+import styles from "style/contents/InvitationMap.module.scss";
+import {
+  NamsanAddr,
+  NamsanLatitude,
+  NamsanLongitude,
+  NamsanPhoneNumber,
+} from "common/WeddingInformation";
 
-    return <div  className={styles.mapWrapper}>
-    <div> 오시는 길</div>
+const InvitationMapComponent: React.FC = () => {
+  const contentResource: contentResource[] = [
+    {
+      resourcePath: mapImage,
+      type: "image/webp",
+    },
+  ];
+  const contentResource2: contentResource[] = [
+    {
+      resourcePath: hanMapImage,
+      type: "image/webp",
+    },
+  ];
 
-    <button onClick={()=>{
-        window.open("https://namsanartweddinghall.com/blogPost/location");
-    }}> 오는길 보기 </button>
-        <button onClick={openKakaoMapApp}> 카카오지도 열기 </button>
-    <ImageComponent resources={contentResource}/>
-    </div>;
-}
+  const handleCallButtonClick = () => {
+    const telLink = `tel:${NamsanPhoneNumber}`;
+    window.location.href = telLink;
+  };
+
+  const openKakaoMapApp = () => {
+    const kakaoMapAppUrl = `kakaomap://look?p=${NamsanLatitude},${NamsanLongitude}`;
+    window.location.href = kakaoMapAppUrl;
+  };
+
+  const openNaverMapApp = () => {
+    const naverMapAppUrl = `nmap://place?lat=${NamsanLatitude}&lng=${NamsanLongitude}&appname=com.nhn.android.nmap`;
+    window.location.href = naverMapAppUrl;
+  };
+
+  const handleCopyButtonClick = async () => {
+    try {
+      await navigator.clipboard.writeText(NamsanAddr);
+      console.log("텍스트가 클립보드에 복사되었습니다.");
+    } catch (err) {
+      console.error("텍스트 복사 중에 오류가 발생했습니다:", err);
+    }
+  };
+
+  return (
+    <div className={styles.mapWrapper}>
+      <div> 오시는 길</div>
+
+      <button
+        onClick={() => {
+          window.open("https://namsanartweddinghall.com/blogPost/location");
+        }}
+      >
+        {"오는길 보기"}
+      </button>
+      <button onClick={openKakaoMapApp}> 카카오지도 열기 </button>
+      <ImageComponent resources={contentResource} />
+      <ImageComponent resources={contentResource2} />
+      <div className={styles.detailInformationWrapper}>
+        <div className={styles.detailInformationPhone}>
+          <div>{`남산예술원 웨딩홀 TEL ${NamsanPhoneNumber}`}</div>
+          <button onClick={handleCallButtonClick}>{"call"}</button>
+        </div>
+        <div className={styles.detailInformationMap}>
+          <div>{NamsanAddr}</div>
+          <button onClick={handleCopyButtonClick}> {"주소 복사하기"}</button>
+        </div>
+        <div className={styles.detailInformationMapWithMapDetail}>
+          <button className={styles.mapGoButtonStyle} onClick={openNaverMapApp}>
+            {"네이버지도 열기"}
+          </button>
+          <button className={styles.mapGoButtonStyle} onClick={openKakaoMapApp}>
+            {"카카오지도 열기"}
+          </button>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
 
 export default InvitationMapComponent;
