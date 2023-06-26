ddd99ebf7b211e1110d29d2c7c4e51ca2598d5d9
dongho_lee <dongho_lee@tmax.co.kr>
2023-06-26 13:38:14 +0900

    [feat] add account Info Component
    - add Account info component
    - fix styles
    - add cmdStyle dialog

--- src/component/MiniCmdComponent.tsx
+++ src/component/MiniCmdComponent.tsx
@@ -1,22 +1,56 @@
-import React from 'react';
-import styles from 'style/MiniCmd.module.scss';
+import React from "react";
+import styles from "style/MiniCmd.module.scss";
 
-const MiniCmdComponent = () => {
-  return (
-    <div className={styles.cmdWindow}>
-      <div className={styles.titleBar}>
-        <div className={styles.titleBarBtns}>
-          <div className={`${styles.titleBarBtn} ${styles.close}`} />
-          <div className={`${styles.titleBarBtn} ${styles.minimize}`} />
-          <div className={`${styles.titleBarBtn} ${styles.expand}`} />
-        </div>
-        <div className={styles.titleBarTitle}>Cmd Window</div>
-      </div>
-      <div className={styles.content}>
-        {/* 내용 */}
-      </div>
-    </div>
-  );
+interface IProps {
+    isOpen: boolean;
+    handleClose: () => void;
+    name: string;
+    account: string;
+    bankName: string;
+}
+
+const MiniCmdComponent: React.FC<IProps> = ({
+    isOpen,
+    handleClose,
+    name,
+    account,
+    bankName,
+}) => {
+    return (
+        <>
+            {isOpen && (
+                <div className={styles.outterWrapper}>
+                    <div className={styles.dialogWrapper}>
+                        <div className={styles.cmdWindow}>
+                            <div className={styles.titleBar}>
+                                <div className={styles.titleBarBtns}>
+                                    <div
+                                        className={`${styles.titleBarBtn} ${styles.close}`}
+                                        onClick={handleClose}
+                                    />
+                                    <div
+                                        className={`${styles.titleBarBtn} ${styles.minimize}`}
+                                        onClick={handleClose}
+                                    />
+                                    <div
+                                        className={`${styles.titleBarBtn} ${styles.expand}`}
+                                        onClick={handleClose}
+                                    />
+                                </div>
+                                <div className={styles.titleBarTitle}>
+                                    {`${name}'s Account Information`}
+                                </div>
+                            </div>
+                            <div className={styles.content}>
+                                {`bank Info: ${bankName}`} <br />
+                                {`account Info: ${account}`} <br />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 };
 
 export default MiniCmdComponent;
--- src/page/invitationDetail/InvitationFundingComponent.tsx
+++ src/page/invitationDetail/InvitationFundingComponent.tsx
@@ -1,21 +1,80 @@
-import React from "react";
+import MiniCmdComponent from "component/MiniCmdComponent";
+import React, { useState } from "react";
 
 import styles from "style/contents/InvitationFunding.module.scss";
 
 const InvitationFundingComponent: React.FC = () => {
-    
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [input, setInputs] = useState<{
+        name: string;
+        account: string;
+        bankName: string;
+    }>({
+        name: "dongho",
+        account: "22040204232474",
+        bankName: "국민",
+    });
+
     return (
-        <div className={styles.wrapper}>
-            <div className={styles.outter}>
-                <div className={styles.grid}></div>
-                <div className={styles.lines}></div>
-                <h1 className={styles.h1Styles}>
-                    <span >Funding</span>
-                    <span  >Funding</span>
-                </h1>
-                <h2 className={styles.h2Style}>Marriage for dongho semi</h2>
+        <>
+            <div className={styles.wrapper}>
+                <div className={styles.buttonOutter}>
+                    <div className={styles.buttonWrapper}>
+                        <div className={styles.buttonItemWrapper}>
+                            <button
+                                className={styles.menButton}
+                                onClick={() => {
+                                    setInputs({
+                                        name: "dongho",
+                                        account: "22040204232474",
+                                        bankName: "국민",
+                                    });
+                                    setDialogOpen(true);
+                                }}
+                            >
+                                {"DONGHO"}
+                            </button>
+                        </div>
+                        <div className={styles.buttonItemWrapper}>
+                            <button
+                                className={styles.womenButton}
+                                onClick={() => {
+                                    setInputs({
+                                        name: "semi",
+                                        account: "11035564626299",
+                                        bankName: "신한",
+                                    });
+                                    setDialogOpen(true);
+                                }}
+                            >
+                                {"SEMI"}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.outter}>
+                    <div className={styles.grid}></div>
+                    <div className={styles.lines}></div>
+                    <h1 className={styles.h1Styles}>
+                        <span>Funding</span>
+                        <span>Funding</span>
+                    </h1>
+                    <h2 className={styles.h2Style}>for dongho semi</h2>
+                </div>
             </div>
-        </div>);
-}
+            {dialogOpen && (
+                <MiniCmdComponent
+                    isOpen={dialogOpen}
+                    handleClose={() => {
+                        setDialogOpen(false);
+                    }}
+                    name={input.name}
+                    account={input.account}
+                    bankName={input.bankName}
+                />
+            )}
+        </>
+    );
+};
 
 export default InvitationFundingComponent;
